86c7dea68441e45e2ed7d1f09a3ae456deed3895
Chrisboy64 <30355637+Chrisboy64@users.noreply.github.com>
2018-02-19 02:22:18 -0500

    Add files via upload
    sql project part 2

--- sql_project.sql
+++ sql_project.sql
@@ -1,5 +1,3 @@
-SELECT SupportRepId, CustomerId, InvoiceId, TrackId, AlbumId
-FROM customers JOIN invoices USING (CustomerId)
-JOIN invoice_items USING (InvoiceId)
-JOIN tracks USING (TrackId)
-WHERE AlbumId IN (8, 21, 22, 23, 24, 25, 26, 27, 28, 29, 32, 33, 34, 41, 42, 45, 47, 52, 53)
+insert into TopCustomer (FirstName, lastname, Address, citi, state, PostalCode)
+select FirstName, LastName, address, city, state, PostalCode
+from customers;
